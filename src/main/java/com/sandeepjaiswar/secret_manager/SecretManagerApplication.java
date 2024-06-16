733a89bc6ce89675cd33674599af8cea218f2431
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2024-06-16 20:55:22 +0530

    chore: setting scratch springboot project

---
+++ src/main/java/com/sandeepjaiswar/secret_manager/SecretManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sandeepjaiswar.secret_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretManagerApplication.class, args);
+	}
+
+}
